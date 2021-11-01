55d019a931ea37f9db3efa527236c10e4ca3ccfa
FabCom <fabcom@communs.fr>
2021-11-01 15:07:26 +0100

    0.2

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
-  belongs_to :commentable
+  belongs_to :commentable, polymorphic: true
   belongs_to :user
   has_many :comments, as: :commentable
   has_many :likes, as: :likeable
