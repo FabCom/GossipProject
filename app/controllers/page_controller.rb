78f2e5f296483acf3f953041aa33f87d4474eddc
FabCom <fabcom@communs.fr>
2021-11-01 18:13:11 +0100

    0.4 avec vues

---
+++ app/controllers/page_controller.rb
@@ -0,0 +1,27 @@
+class PageController < ApplicationController
+
+  def home
+    if params[:name] == "" || params[:name] == nil
+      @name = "Je ne connais pas ton blaze, je n'ai pas pu personnaliser ce message..."
+      @path = "=> http://localhost:3000/home/TonBlaze"
+    else
+      @name = params[:name]
+    end
+  end
+
+  def index
+    @gossips = Gossip.all
+  end
+
+  def users
+    @users = User.all
+  end
+
+  def team
+    @team = ["Tim","Got","Louis","Xav"]
+  end
+
+  def contact
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+
+  root "page#index"
+
+  get '/home/:name', to: "page#home"
+
+  get '/home', to: "page#home", as: 'home'
+
+  get '/index', to: "page#index", as: 'index'
+
+  get '/users', to: "page#users", as: 'users'
+
+  get '/team', to: "page#team", as: 'team'
+
+  get '/contact', to: "page#contact", as: 'contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
